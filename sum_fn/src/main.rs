3189dcb473d8ddad256bc5e88fd7b3f3c4233d4b
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2023-01-28 15:32:15 -0300

    update rust exercises

---
+++ sum_fn/src/main.rs
@@ -0,0 +1,7 @@
+fn sum(first_number: i32, second_number: i32) -> i32 {
+    first_number + second_number
+}
+
+fn main() {
+    println!("{}", sum(2, 3));
+}
--- test/test.rs
+++
@@ -1,7 +0,0 @@
-fn add2(first_number: i32, second_number:i32 ) -> i32 {
-    first_number + second_number
-}
-
-fn main() {
-    println!("{}", add2(2,3));
-}
